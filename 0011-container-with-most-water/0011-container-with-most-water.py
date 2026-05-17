990b1c7865182d6ee02f6d12462c88786dfd0ad7
Seongwon Jang <jungseonglian@sju.ac.kr>
2026-05-17 14:42:15 +0900

    Time: 51 ms (85.52%), Space: 30.2 MB (5.45%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -4,10 +4,11 @@ class Solution:
         left, right = 0, len(height)-1
 
         while left < right :
-            mh = height[left] if min(height[left], height[right]) == height[left] else height[right]
+            hl, hr = height[left], height[right]
+            mh = hl if min(hl, hr) == hl else hr
             area.append(mh*(right-left))
 
-            if mh == height[left] :
+            if mh == hl :
                 left += 1
             else :
                 right -= 1
