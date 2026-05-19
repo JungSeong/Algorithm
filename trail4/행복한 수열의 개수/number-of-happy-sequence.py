275331f6e4e70c2e00f00cfd29214f02d277c281
Seongwon Jang <jungseonglian@sju.ac.kr>
2026-05-19 15:51:06 +0900

    [Passed] 행복한 수열의 개수 (75ms, 17MB)

--- trail4/행복한 수열의 개수/number-of-happy-sequence.py
+++ trail4/행복한 수열의 개수/number-of-happy-sequence.py
@@ -1,30 +1,22 @@
 n, m = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(n)]
+ans = 0
 
 # Please write your code here.
-ans = 0
-from collections import Counter
+from itertools import groupby
 
-def cnt(grid) :
+def cnt(grid, m) :
     ans = 0
     for row in grid :
-        length = len(row)
-        cnt = 0
-        before = -1
-        for i in range(length) :
-            if before != row[i] :
-                cnt = 1
-                before = row[i]
-            else :
-                cnt += 1
-            
-            if cnt >= m :
+        for _, group in groupby(row) :
+            if len(list(group)) >= m :
                 ans += 1
                 break
-
+    
     return ans
 
-ans += cnt(grid)
-rgrid = [row for row in map(list, zip(*grid))]
-ans += cnt(rgrid)
+ans += cnt(grid, m)
+rgrid = list(map(list, zip(*grid)))
+ans += cnt(rgrid, m)
+
 print(ans)
