215dfc3cf6601cb76ea9acb2b6e274713a8078b9
Seongwon Jang <jungseonglian@sju.ac.kr>
2026-08-28 10:25:01 +0900

    Time: 902 ms (5.04%), Space: 249 MB (9.67%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        if (n <= 2) return 0;
+
+        vector<char> isPrime(n, 1);
+        isPrime[0] = 0;
+        isPrime[1] = 0;
+
+        for (int i = 2; 1LL * i * i < n; i++) {
+            if (isPrime[i]) {
+                for (long long j = 1LL * i * i; j < n; j += i) {
+                    isPrime[j] = 0;
+                }
+            }
+        }
+
+        int answer = 0;
+
+        for (int i = 2; i < n; i++) {
+            answer += isPrime[i];
+        }
+
+        return answer;
+    }
+};
